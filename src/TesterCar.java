6810af03d63e6139826fb676b3372839ff34b0e9
suleoz <suleozdemir107@gmail.com>
2021-11-03 20:14:11 -0400

    the red freerarri car on the board

--- src/TesterCar.java
+++ src/TesterCar.java
@@ -3,7 +3,14 @@ public class TesterCar {
     public static void main(String[] args) {
 
         car newCar=new car();
-        
+
+        newCar.name="Feerarii";
+        newCar.color="Red";
+        newCar.doors=3;
+        newCar.engineCapacity="60000cc";
+
+        System.out.println(newCar.color+" "+newCar.name);
+
 
 
     }
